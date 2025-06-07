501eea51bad7f9d9acced9d41cf2f96352b00e0e
sansarov <st.sansarov@gmail.com>
2025-06-07 14:11:19 +0300

    Update guess_a_number_by_slavy.py

--- guess_a_number_by_slavy.py
+++ guess_a_number_by_slavy.py
@@ -13,6 +13,114 @@ while True:
 
         if player_number == computer_number:
             print("You guess it!")
+            computer_number = random.randint(1, 200)
+            while True:
+                player_input = input("Guess the number (1-200): ")
+
+                if not player_input.isdigit():
+                    print("Invalid input. Try again...")
+                    continue
+                else:
+                    player_number = int(player_input)
+
+                    if player_number == computer_number:
+                        print("You guess it!")
+                        computer_number = random.randint(1, 500)
+
+                        while True:
+                            player_input = input("Guess the number (1-500): ")
+
+                            if not player_input.isdigit():
+                                print("Invalid input. Try again...")
+                                continue
+                            else:
+                                player_number = int(player_input)
+
+                                if player_number == computer_number:
+                                    print("You guess it!")
+                                    computer_number = random.randint(1, 1000)
+
+                                    while True:
+                                        player_input = input("Guess the number (1-1000): ")
+
+                                        if not player_input.isdigit():
+                                            print("Invalid input. Try again...")
+                                            continue
+                                        else:
+                                            player_number = int(player_input)
+
+                                            if player_number == computer_number:
+                                                print("You guess it!")
+                                                computer_number = random.randint(1, 2000)
+
+                                                while True:
+                                                    player_input = input("Guess the number (1-2000): ")
+
+                                                    if not player_input.isdigit():
+                                                        print("Invalid input. Try again...")
+                                                        continue
+                                                    else:
+                                                        player_number = int(player_input)
+
+                                                        if player_number == computer_number:
+                                                            print("You guess it!")
+                                                            computer_number = random.randint(1, 5000)
+
+                                                            while True:
+                                                                player_input = input("Guess the number (1-5000): ")
+
+                                                                if not player_input.isdigit():
+                                                                    print("Invalid input. Try again...")
+                                                                    continue
+                                                                else:
+                                                                    player_number = int(player_input)
+
+                                                                    if player_number == computer_number:
+                                                                        print("You guess it!")
+                                                                        computer_number = random.randint(1, 10000)
+
+                                                                        while True:
+                                                                            player_input = input(
+                                                                                "Guess the number (1-10000): ")
+
+                                                                            if not player_input.isdigit():
+                                                                                print("Invalid input. Try again...")
+                                                                                continue
+                                                                            else:
+                                                                                player_number = int(player_input)
+
+                                                                                if player_number == computer_number:
+                                                                                    print("You guess it!")
+                                                                                    break
+                                                                                elif player_number > computer_number:
+                                                                                    print("Too High!")
+                                                                                else:
+                                                                                    print("Too Low!")
+                                                                        break
+                                                                    elif player_number > computer_number:
+                                                                        print("Too High!")
+                                                                    else:
+                                                                        print("Too Low!")
+                                                            break
+                                                        elif player_number > computer_number:
+                                                            print("Too High!")
+                                                        else:
+                                                            print("Too Low!")
+                                                break
+                                            elif player_number > computer_number:
+                                                print("Too High!")
+                                            else:
+                                                print("Too Low!")
+                                    break
+                                elif player_number > computer_number:
+                                    print("Too High!")
+                                else:
+                                    print("Too Low!")
+                        break
+                    elif player_number > computer_number:
+                        print("Too High!")
+                    else:
+                        print("Too Low!")
             break
         elif player_number > computer_number:
             print("Too High!")
